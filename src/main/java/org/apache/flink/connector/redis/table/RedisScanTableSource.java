404304c82c83b68e055b4bbeaac1c02a74fc6632
liuyang <98247607+luna-learn@users.noreply.github.com>
2023-04-22 23:28:32 +0800

    fix: 在 redis source 和 sink 中增加 io metric 功能

--- src/main/java/org/apache/flink/connector/redis/table/RedisScanTableSource.java
+++ src/main/java/org/apache/flink/connector/redis/table/RedisScanTableSource.java
@@ -1,10 +1,16 @@
 package org.apache.flink.connector.redis.table;
 
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.redis.config.RedisConnectionOptions;
 import org.apache.flink.connector.redis.config.RedisSourceOptions;
 import org.apache.flink.connector.redis.container.RedisContainer;
 import org.apache.flink.connector.redis.mapper.RedisMapper;
+import org.apache.flink.metrics.groups.OperatorIOMetricGroup;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.apache.flink.table.data.RowData;
 import redis.clients.jedis.params.ScanParams;
@@ -14,12 +20,15 @@ import redis.clients.jedis.params.ScanParams;
  * @author Liu Yang
  * @date 2023/1/11 9:20
  */
-public class RedisScanTableSource extends RichSourceFunction<RowData> {
+public class RedisScanTableSource extends RichSourceFunction<RowData> implements CheckpointedFunction {
 
     private final RedisConnectionOptions options;
     private final RedisMapper<RowData> mapper;
     private final RedisSourceOptions sourceOptions;
     private RedisContainer container;
+    private OperatorIOMetricGroup ioMetric;
+    private long readRecords = 0;
+    private ListState<Long> readRecordState;
 
     public RedisScanTableSource(RedisConnectionOptions options,
                                 RedisMapper<RowData> mapper,
@@ -34,13 +43,18 @@ public class RedisScanTableSource extends RichSourceFunction<RowData> {
     public void open(Configuration parameters) throws Exception {
         this.container = options.getContainer();
         this.container.open();
+        this.ioMetric = getRuntimeContext().getMetricGroup().getIOMetricGroup();
     }
 
     @Override
     public void run(SourceContext<RowData> ctx) throws Exception {
         ScanParams params = new ScanParams().count(10);
         this.mapper.scan(this.container, params, row -> {
+            ioMetric.getNumRecordsInCounter().inc();
             ctx.collectWithTimestamp(row, System.currentTimeMillis());
+            ioMetric.getNumRecordsOutCounter().inc();
+            readRecords++;
+            ctx.markAsTemporarilyIdle();
         });
     }
 
@@ -55,6 +69,22 @@ public class RedisScanTableSource extends RichSourceFunction<RowData> {
     public void cancel() {
         if (this.container != null) {
             this.container.close();
+        }
+    }
+
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) throws Exception {
+        readRecordState.clear();
+        readRecordState.add(readRecords);
+    }
+
+    @Override
+    public void initializeState(FunctionInitializationContext context) throws Exception {
+        readRecordState = context.getOperatorStateStore().getListState(new ListStateDescriptor<>("ReadRecords", Long.class));
+        if (context.isRestored()) {
+            for(Long value: readRecordState.get()) {
+                readRecords = value;
+            }
         }
     }
 }
--- src/main/java/org/apache/flink/connector/redis/table/RedisTableSink.java
+++ src/main/java/org/apache/flink/connector/redis/table/RedisTableSink.java
@@ -1,11 +1,17 @@
 package org.apache.flink.connector.redis.table;
 
 import org.apache.flink.api.common.eventtime.Watermark;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.redis.config.RedisConnectionOptions;
 import org.apache.flink.connector.redis.config.RedisSinkOptions;
 import org.apache.flink.connector.redis.container.RedisContainer;
 import org.apache.flink.connector.redis.mapper.RedisMapper;
+import org.apache.flink.metrics.groups.OperatorIOMetricGroup;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.data.StringData;
@@ -16,11 +22,15 @@ import org.apache.flink.table.data.StringData;
  * @author Liu Yang
  * @date 2023/1/11 14:05
  */
-public class RedisTableSink extends RichSinkFunction<RowData> {
+public class RedisTableSink extends RichSinkFunction<RowData> implements CheckpointedFunction {
     private final RedisConnectionOptions options;
     private final RedisMapper<RowData> mapper;
     private final RedisSinkOptions sinkOptions;
     private RedisContainer container;
+    private OperatorIOMetricGroup ioMetric;
+    private long writeRecords = 0;
+
+    private ListState<Long> writeRecordState;
 
     public RedisTableSink(RedisConnectionOptions options,
                           RedisMapper<RowData> mapper,
@@ -34,6 +44,7 @@ public class RedisTableSink extends RichSinkFunction<RowData> {
     public void open(Configuration parameters) throws Exception {
         this.container = options.getContainer();
         this.container.open();
+        ioMetric =  getRuntimeContext().getMetricGroup().getIOMetricGroup();
     }
 
     @Override
@@ -46,8 +57,10 @@ public class RedisTableSink extends RichSinkFunction<RowData> {
             return ;
         }
         try {
-            this.mapper.upsert(this.container, pk.toString(), value);
-            this.writeWatermark(new Watermark(System.currentTimeMillis()));
+            mapper.upsert(this.container, pk.toString(), value);
+            ioMetric.getNumRecordsOutCounter().inc();
+            writeRecords++;
+            writeWatermark(new Watermark(System.currentTimeMillis()));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -61,4 +74,19 @@ public class RedisTableSink extends RichSinkFunction<RowData> {
         }
     }
 
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) throws Exception {
+        writeRecordState.clear();
+        writeRecordState.add(writeRecords);
+    }
+
+    @Override
+    public void initializeState(FunctionInitializationContext context) throws Exception {
+        writeRecordState = context.getOperatorStateStore().getListState(new ListStateDescriptor<>("WriteRecords", Long.class));
+        if (context.isRestored()) {
+            for(Long value: writeRecordState.get()) {
+                writeRecords = value;
+            }
+        }
+    }
 }
